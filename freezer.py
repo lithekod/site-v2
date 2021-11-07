a9381e360d7439badd5466172e025cf85d573821
Erik Mattfolk <erik.mattfolk@gmail.com>
2021-11-07 21:43:57 +0100

    Format the python files using black

--- freezer.py
+++ freezer.py
@@ -3,5 +3,5 @@ from website import app
 
 freezer = Freezer(app, with_static_files=True)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     freezer.freeze()
--- website/__init__.py
+++ website/__init__.py
@@ -12,24 +12,25 @@ from flask import (
     url_for,
 )
 
-app = Flask(__name__, static_folder='static')
+app = Flask(__name__, static_folder="static")
 
 # ========== Navigation ==========
 # Elements used to create navigation bar
 # ((name_se, name_en), url)
 
 navigation = [
-    (("Start",        "Start"       ), "/"             ),
-    (("Kontakt",      "Contact us"  ), "/contact/"     ),
-    (("Tävlingar",    "Competitions"), "/competitions/"),
-    (("Game Jam",     "Game Jam"    ), "/gamejam/"     ),
+    (("Start", "Start"), "/"),
+    (("Kontakt", "Contact us"), "/contact/"),
+    (("Tävlingar", "Competitions"), "/competitions/"),
+    (("Game Jam", "Game Jam"), "/gamejam/"),
     (("Organisation", "Organization"), "/organization/"),
-    (("Fusk",         "Cheats"      ), "/cheats/"      ),
+    (("Fusk", "Cheats"), "/cheats/"),
 ]
 
 # ========== Helper functions ==========
 # These functions make it easy to render files into pages or
 # redirecting to other pages.
+
 
 def last_updated(path):
     """Return the author date of the latest commit that touched a path."""
@@ -50,14 +51,16 @@ def render_page(path, url, swedish, injection=""):
     swedish - Whether the page is in swedish or not (english).
     """
     nav_index = next((i for i, (_, u) in enumerate(navigation) if u == url), -1)
-    return render_template("page.html",
-            html=markdown2.markdown_path(path),
-            injection=injection,
-            last_updated=last_updated(path),
-            url=url,
-            navigation=navigation,
-            selected=nav_index,
-            swedish=swedish)
+    return render_template(
+        "page.html",
+        html=markdown2.markdown_path(path),
+        injection=injection,
+        last_updated=last_updated(path),
+        url=url,
+        navigation=navigation,
+        selected=nav_index,
+        swedish=swedish,
+    )
 
 
 def static_page(path):
@@ -79,13 +82,14 @@ def redirect_external(url):
 
 
 def create_view(md_file, url, swedish):
-    """ Return a function that returns a page. """
+    """Return a function that returns a page."""
     return lambda: render_page(md_file, url, swedish)
 
 
 def create_redirect(to):
-    """ Return a function that returns a redirect. """
+    """Return a function that returns a redirect."""
     return lambda: redirect(to)
+
 
 # ========== Temporary pages ==========
 # These pages should be removed when apropriate.
@@ -94,30 +98,32 @@ def create_redirect(to):
 # ========== Redirects ==========
 # Redirect to the snake-ribs documentation.
 
+
 @app.route("/snake-ribs")
 @app.route("/snake-ribs/")
 def snake_ribs():
     return redirect_external("https://lithekod.github.io/snake-ribs/")
 
+
 # ========== Pages ==========
 # These are the main pages on the LiTHe kod website.
 
 pages = [
-    ("/",                 "website/pages/index_{}.md"),
-    ("/contact/",         "website/pages/contact_{}.md"),
-    ("/competitions/",    "website/pages/competitions_{}.md"),
-    ("/gamejam/",         "website/pages/gamejam_{}.md"),
+    ("/", "website/pages/index_{}.md"),
+    ("/contact/", "website/pages/contact_{}.md"),
+    ("/competitions/", "website/pages/competitions_{}.md"),
+    ("/gamejam/", "website/pages/gamejam_{}.md"),
     ("/gamejam/history/", "website/pages/gamejam_history_{}.md"),
-    ("/gamejam/tools/",   "website/pages/gamejam_tools_{}.md"),
-    ("/organization/",    "website/pages/organization_{}.md"),
-    ("/cheats/",          "website/pages/cheats_{}.md"),
-    ("/ncpc/",            "website/pages/ncpc_{}.md"),
-    ("/impa/",            "website/pages/impa_{}.md"),
-    ("/aoc/",             "website/pages/aoc_{}.md"),
-    ("/codingcup/",       "website/pages/codingcup_{}.md"),
-    ("/microjam/",        "website/pages/microjam_{}.md"),
-    ("/meetings/",        "website/pages/meetings_{}.md"),
-    ("/git/",             "website/pages/git_{}.md"),
+    ("/gamejam/tools/", "website/pages/gamejam_tools_{}.md"),
+    ("/organization/", "website/pages/organization_{}.md"),
+    ("/cheats/", "website/pages/cheats_{}.md"),
+    ("/ncpc/", "website/pages/ncpc_{}.md"),
+    ("/impa/", "website/pages/impa_{}.md"),
+    ("/aoc/", "website/pages/aoc_{}.md"),
+    ("/codingcup/", "website/pages/codingcup_{}.md"),
+    ("/microjam/", "website/pages/microjam_{}.md"),
+    ("/meetings/", "website/pages/meetings_{}.md"),
+    ("/git/", "website/pages/git_{}.md"),
 ]
 
 for url, md_file in pages:
@@ -139,54 +145,68 @@ for url, md_file in pages:
 # These pages can be accessed from a direct link. They do not show up
 # on the sidebar.
 
+
 @app.route("/gitcheatsheet/")
 def gitcheatsheet():
-    """ The git cheat-sheet of doom! """
+    """The git cheat-sheet of doom!"""
     return static_page("website/other/gitcheatsheet.html")
+
 
 @app.route("/vimrc")
 def vimrc():
-    """ Get the vimrc. """
+    """Get the vimrc."""
     return send_file("other/vimrc", attachment_filename=".vimrc", as_attachment=True)
+
 
 @app.route("/emacs_config")
 def emacs():
-    """ Get the emacs config. """
-    return send_file("other/emacs_config", attachment_filename=".emacs", as_attachment=True)
+    """Get the emacs config."""
+    return send_file(
+        "other/emacs_config", attachment_filename=".emacs", as_attachment=True
+    )
+
 
 @app.route("/lacc/")
 def lacc():
-    """ LiTHe kod's Amazing Coding Challenges """
+    """LiTHe kod's Amazing Coding Challenges"""
     return static_page("website/other/lacc.html")
+
 
 # ========== Old redirects ==========
 # These pages used to exist, but not anymore. They redirect to the new content
 # so old links still work.
 
+
 @app.route("/posts/")
 def posts_index():
     return redirect("/meetings/se/")
+
 
 @app.route("/posts/se/")
 def posts_se():
     return redirect("/meetings/se/")
 
+
 @app.route("/posts/en/")
 def posts_en():
     return redirect("/meetings/en/")
 
+
 # ========== Errorhandlers ==========
 # For now we only handle pages that are not found.
 
+
 @app.errorhandler(404)
 def not_found(e):
-    """ 404 Page """
+    """404 Page"""
     return render_page("website/pages/404.md", "/404/", False), 404
+
 
 @app.route("/404.html")
 def not_found_gh_pages():
-    """ 404 page to please GitHub pages """
+    """404 page to please GitHub pages"""
     return render_page("website/pages/404.md", "/404/", False)
+
 
 # ========== Running ==========
 # Running this file will allow nonlocal devices to access the page.
