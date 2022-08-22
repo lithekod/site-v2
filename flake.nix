8dfebbe516d9c5d42a67d35ee00ef51621e75193
NomisIV <simon@nomisiv.com>
2022-08-22 21:36:19 +0200

    Use mach-nix in flake

--- flake.nix
+++ flake.nix
@@ -1,12 +1,16 @@
 {
   description = "A very basic flake";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-  outputs = { self, nixpkgs }: let
-    pkgs = nixpkgs.legacyPackages.x86_64-linux;
-  in {
-    devShells.x86_64-linux.default = pkgs.mkShell {
-      buildInputs = with pkgs; [ python3 ];
+  inputs.mach-nix.url = "github:davhau/mach-nix";
+  outputs = {self, mach-nix}: {
+    packages.x86_64-linux.default = mach-nix.lib.x86_64-linux.buildPythonApplication {
+      pname = "lithekod-website";
+      version = "0.1.0";
+      src = ./.;
+      requirements = builtins.readFile ./requirements.txt;
+    };
+    devShells.x86_64-linux.default = mach-nix.lib.x86_64-linux.mkPythonShell {
+      requirements = builtins.readFile ./requirements.txt;
     };
   };
 }
